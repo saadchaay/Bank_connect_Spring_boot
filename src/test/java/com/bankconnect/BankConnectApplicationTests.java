d5dd1146d50a358b5f066fe197a9f0ba31f91d23
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-12-15 09:40:02 +0100

    project setup

---
+++ src/test/java/com/bankconnect/BankConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
