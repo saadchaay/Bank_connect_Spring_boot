9b6686a4012d8a965a7f4b4c76f81eea08fac4fc
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-12-16 09:58:33 +0100

    rest api test response

---
+++ src/main/java/com/bankconnect/controllers/TransactionController.java
@@ -0,0 +1,28 @@
+package com.bankconnect.controllers;
+
+import com.bankconnect.services.AgentService;
+import jakarta.persistence.Entity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("transactions")
+public class TransactionController {
+
+    private final AgentService agentService;
+
+    public TransactionController(AgentService agentService) {
+        this.agentService = agentService;
+    }
+
+    @GetMapping("all")
+    public ResponseEntity<List> transactions(){
+        return ResponseEntity.ok(agentService.listAll());
+    }
+}
