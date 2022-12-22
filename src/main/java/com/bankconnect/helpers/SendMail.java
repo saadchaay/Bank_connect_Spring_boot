136107d23a1cc37f4865d1da025035ff2a3d74a1
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-12-22 12:01:01 +0100

    Add method to send bill to client

--- src/main/java/com/bankconnect/helpers/SendMail.java
+++ src/main/java/com/bankconnect/helpers/SendMail.java
@@ -2,23 +2,17 @@ package com.bankconnect.helpers;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
-
-import java.io.File;
-import java.util.Properties;
 
 @Component
 public class SendMail {
 
     private final JavaMailSender emailSender;
-    private final String form = "samirnouami@gmail.com";
+    private final String from = "samirnouami@gmail.com";
 
     public SendMail(JavaMailSender emailSender) {
         this.emailSender = emailSender;
@@ -40,8 +34,8 @@ public class SendMail {
 
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
-        helper.setFrom("");
-        helper.setTo(form);
+        helper.setFrom(from);
+        helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(text);
 
